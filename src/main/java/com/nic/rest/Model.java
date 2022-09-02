578b47f9d660044ee339894392e867e068f1135f
Admin <ramkumar180499@gmail.com>
2022-09-02 16:36:57 +0530

    first commit

---
+++ src/main/java/com/nic/rest/Model.java
@@ -0,0 +1,14 @@
+package com.nic.rest;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Model {
+	private int user_id;
+	private String user_name,password;
+}
